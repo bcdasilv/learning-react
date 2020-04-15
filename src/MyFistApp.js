5528d39269a218cb4697db285068f5dc6f388fe3
Bruno da Silva <brunocarreiro@gmail.com>
2020-04-14 19:16:40 -0700

    Implementing fetch all and post operations in the frontend

--- src/MyFistApp.js
+++ src/MyFistApp.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 class MyFirstApp extends Component {
     state = {
         people : [],
     }
 
+    componentDidMount() {
+        axios.get(`http://localhost:5000/users`)
+          .then(res => {
+            const people = res.data.users_list;
+            this.setState({ people });
+          })
+          .catch(function (error) {
+            //Not handling the error. Just logging in the console.
+            console.log(error);
+          });
+      }
+
     removePerson = index => {
         const { people } = this.state
-      
+
         this.setState({
           people: people.filter((person, i) => {
             return i !== index;
@@ -17,8 +30,24 @@ class MyFirstApp extends Component {
         })
     }
 
+    makePostCall(person){
+        return axios.post('http://localhost:5000/users', person)
+        .then(function (response) {
+          console.log(response);
+          return (response.status === 200);
+        })
+        .catch(function (error) {
+          console.log(error);
+          return false;
+        });
+    }
+
     handleSubmit = person => {
-        this.setState({ people: [...this.state.people, person] })
+        this.makePostCall(person).then( callResult => {
+            if (callResult === true) {
+                this.setState({ people: [...this.state.people, person] });
+            }
+        });
     }
 
     render() {
